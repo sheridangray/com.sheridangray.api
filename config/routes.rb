46925a3a0c14c8cd3d2a53c427d8107f209db5e6
sheridangray <sheridan.gray@gmail.com>
2017-08-23 08:54:34 -0700

    Sets up rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
-SheridanGrayApi.application.routes.draw do
+require 'api_constraints'
+
+ComSheridangrayApi::Application.routes.draw do
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: 'api' }, 
                   path: '/' do
+
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
 
   end
 end
